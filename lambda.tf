7675eb8cc08d8d8868949ff34532caebb9914808
Adam Christie <fractos@gmail.com>
2019-09-13 14:03:15 +0100

    remove source file hash for s3 implementation.

--- lambda.tf
+++ lambda.tf
@@ -24,13 +24,12 @@ resource "aws_lambda_permission" "allow_sns_to_call_sns_to_slack_file" {
 }
 
 resource "aws_lambda_function" "sns_to_slack_s3" {
-  count            = "${length(var.source_package) > 0 ? 0 : 1}"
-  function_name    = "${var.prefix}-sns-to-slack"
-  handler          = "main.lambda_handler"
-  runtime          = "python2.7"
-  s3_bucket        = "${var.s3_bucket}"
-  s3_key           = "${var.s3_key}"
-  source_code_hash = "${base64sha256(file(var.source_package))}"
+  count         = "${length(var.source_package) > 0 ? 0 : 1}"
+  function_name = "${var.prefix}-sns-to-slack"
+  handler       = "main.lambda_handler"
+  runtime       = "python2.7"
+  s3_bucket     = "${var.s3_bucket}"
+  s3_key        = "${var.s3_key}"
 
   role = "${aws_iam_role.sns_to_slack_exec_role.arn}"
 
