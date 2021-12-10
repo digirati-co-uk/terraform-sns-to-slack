d2d41a9d7531636a96110f881db4053a83ae2db4
Donald Gray <donald.gray@digirati.com>
2021-12-10 11:43:18 +0000

    Remove count usage from s3

--- s3/lambda.tf
+++ s3/lambda.tf
@@ -1,5 +1,4 @@
 resource "aws_lambda_function" "sns_to_slack_s3" {
-  count         = length(var.source_package) > 0 ? 0 : 1
   function_name = "${var.prefix}-sns-to-slack"
   handler       = "main.lambda_handler"
   runtime       = "python3.9"
@@ -16,9 +15,8 @@ resource "aws_lambda_function" "sns_to_slack_s3" {
 }
 
 resource "aws_lambda_permission" "allow_sns_to_call_sns_to_slack_s3" {
-  count         = length(var.source_package) > 0 ? 0 : 1
   statement_id  = "AllowExecutionFromSNS"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.sns_to_slack_s3[0].function_name
+  function_name = aws_lambda_function.sns_to_slack_s3.function_name
   principal     = "sns.amazonaws.com"
 }
